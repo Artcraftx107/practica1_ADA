a29e4fc3747f69bbcd4eaeaff6a59afc168c9865
Artcraft <arturoao@uma.es>
2024-10-03 16:28:48 +0200

    Added solution for exercise 1 of the practice

---
+++ src/main/java/p1/DosCasos.java
@@ -0,0 +1,22 @@
+package p1;
+
+public class DosCasos extends Metodo{
+    public DosCasos(){
+        super(Orden.CTE, Orden.N);
+    }
+
+    @Override
+    public int codigo(int n){
+        //Iniciamos code en 1, ya que si n==0 se devuelve 1 como codigo
+        int code = 1;
+        //Peor caso, en el mejor caso se salta el if
+        if(n!=0){
+            code=0;
+            for(int i = 0; i<n; i++){
+                code+=i;
+            }
+        }
+
+        return code;
+    }
+}
