43309e46d3dec990939a03f73f1e17b34cd25694
Artcraft <arturoao@uma.es>
2024-10-03 16:05:17 +0200

    Added base files for the practical exercise

---
+++ src/main/java/p1/ProcesaLista.java
@@ -0,0 +1,36 @@
+package p1;
+
+import java.util.List;
+
+public class ProcesaLista extends Metodo{
+	private List<Integer> lista;
+	
+	public ProcesaLista() {
+		super();
+		lista = null;
+		
+	}
+	public void setLista(List<Integer> l) {
+		lista = l;
+	}
+		
+	public List<Integer> getLista() {
+		return lista;
+	}
+	
+	/**
+	 * Procesamos todos los elementos de la lista lista.
+	 * return El número de elementos procesados en realidad.
+	 */
+	@Override
+	public int codigo(int n) {
+		procesaLista(lista);
+		return n>lista.size()?n:lista.size();
+	}
+	
+	private void procesaLista(List<Integer> lista) {
+		//Completar la implementación del método
+	}
+	
+
+}
