b9705cb2107ee0a4819333c85eca2d0bd80f46f5
Artcraft <arturoao@uma.es>
2024-10-21 17:50:34 +0200

    Checking for changes

--- src/main/java/ProcesaLista.java
+++ src/main/java/ProcesaLista.java
@@ -3,20 +3,20 @@ import java.util.List;
 
 public class ProcesaLista extends Metodo {
 	private List<Integer> lista;
-	
+
 	public ProcesaLista() {
 		super();
 		lista = null;
-		
+
 	}
 	public void setLista(List<Integer> l) {
 		lista = l;
 	}
-		
+
 	public List<Integer> getLista() {
 		return lista;
 	}
-	
+
 	/**
 	 * Procesamos todos los elementos de la lista lista.
 	 * return El número de elementos procesados en realidad.
@@ -26,7 +26,7 @@ public class ProcesaLista extends Metodo {
 		procesaLista(lista);
 		return n>lista.size()?n:lista.size();
 	}
-	
+
 	private void procesaLista(List<Integer> lista) {
 		if(lista==null||lista.size()<=1){
 			System.err.println("No hay repetidos a eliminar");
